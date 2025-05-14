8c9cb46143a36f653ad3b91e7346019db75513da
lllmaclll <makky1593@gmail.com>
2025-05-14 21:11:47 +0700

    add plugin swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
 import { aqiIndoorRoute } from './routes/aqiIndoor.route';
 import { aqiOutdoorRoute } from './routes/aqiOutdoor.route';
 
 const app = new Elysia()
   .use(cors())
+  .use(swagger())
   .use(aqiOutdoorRoute)
   .use(aqiIndoorRoute)
   .listen(3001);
